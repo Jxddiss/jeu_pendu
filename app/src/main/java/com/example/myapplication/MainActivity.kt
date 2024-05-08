a3ace2d1e627e56c69ba12ad8535e56d784f74e3
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-08 11:44:17 -0400

    MAin activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnStart: Button
+
+    companion object {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnJeu)
+
+        btnStart.setOnClickListener {
+            val intent: Intent = Intent(this, PenduJeuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

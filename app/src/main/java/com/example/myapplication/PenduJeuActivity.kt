2e2c25394f37f47073e1a8a3fb312e33aa015ad3
Nicholson Rainville Jacques <1875595@crosemont.qc.ca>
2024-05-08 11:47:45 -0400

    Pendue play gif

--- app/src/main/java/com/example/myapplication/PenduJeuActivity.kt
+++ app/src/main/java/com/example/myapplication/PenduJeuActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
-class penduJeuActivity : AppCompatActivity() {
+class PenduJeuActivity : AppCompatActivity() {
+    lateinit var gifImageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pendu_jeu)
+
+        gifImageView = findViewById(R.id.animGifImageView)
     }
+
+
 }
